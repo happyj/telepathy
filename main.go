39363f07e85bbef47f0976f657e957d9c2f0e558
happyj <hejun0213@gmail.com>
2014-12-14 16:31:30 +0800

    telepathy api model
    telepathy api model

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "telepathy/docs"
+	_ "telepathy/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.RunMode == "dev" {
+		beego.DirectoryIndex = true
+		beego.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
