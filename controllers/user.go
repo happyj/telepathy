3db180e4209a795259725de7f2d525b1d2a87268
happyj <hejun0213@gmail.com>
2014-12-27 15:38:25 +0800

    注册登录流程 -1
    注册登录流程 -1

--- controllers/user.go
+++ controllers/user.go
@@ -1,93 +1,80 @@
 package controllers
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"telepathy/models"
 
-	"github.com/astaxie/beego"
+	//"github.com/astaxie/beego"
 )
 
 // Operations about Users
 type UserController struct {
-	beego.Controller
+	CommonController
 }
 
-// @Title createUser
-// @Description create users
-// @Param	body		body 	models.User	true		"body for user content"
-// @Success 200 {int} models.User.Id
-// @Failure 403 body is empty
-// @router / [post]
-func (u *UserController) Post() {
-	var user models.User
-	json.Unmarshal(u.Ctx.Input.RequestBody, &user)
-	uid := models.AddUser(user)
-	u.Data["json"] = map[string]string{"uid": uid}
-	u.ServeJson()
-}
-
-// @Title Get
-// @Description get all Users
-// @Success 200 {object} models.User
-// @router / [get]
-func (u *UserController) GetAll() {
-	users := models.GetAllUsers()
-	u.Data["json"] = users
-	u.ServeJson()
-}
-
-// @Title Get
-// @Description get user by uid
-// @Param	uid		path 	string	true		"The key for staticblock"
-// @Success 200 {object} models.User
-// @Failure 403 :uid is empty
-// @router /:uid [get]
-func (u *UserController) Get() {
-	uid := u.GetString(":uid")
-	if uid != "" {
-		user, err := models.GetUser(uid)
-		if err != nil {
-			u.Data["json"] = err
-		} else {
-			u.Data["json"] = user
-		}
+func (this *UserController) Register() {
+	u := models.User{}
+	if err := this.ParseForm(&u); err != nil {
+		//handle error
+		this.Rsp(false, err.Error())
+		return
 	}
-	u.ServeJson()
-}
-
-// @Title update
-// @Description update the user
-// @Param	uid		path 	string	true		"The uid you want to update"
-// @Param	body		body 	models.User	true		"body for user content"
-// @Success 200 {object} models.User
-// @Failure 403 :uid is not int
-// @router /:uid [put]
-func (u *UserController) Put() {
-	uid := u.GetString(":uid")
-	if uid != "" {
-		var user models.User
-		json.Unmarshal(u.Ctx.Input.RequestBody, &user)
-		uu, err := models.UpdateUser(uid, &user)
-		if err != nil {
-			u.Data["json"] = err
-		} else {
-			u.Data["json"] = uu
-		}
+	err := models.Register(&u)
+	if err == nil {
+		this.Rsp(true, "Success")
+		return
+	} else {
+		this.Rsp(false, err.Error())
+		return
 	}
-	u.ServeJson()
+
 }
 
-// @Title delete
-// @Description delete the user
-// @Param	uid		path 	string	true		"The uid you want to delete"
-// @Success 200 {string} delete success!
-// @Failure 403 uid is empty
-// @router /:uid [delete]
-func (u *UserController) Delete() {
-	uid := u.GetString(":uid")
-	models.DeleteUser(uid)
-	u.Data["json"] = "delete success!"
-	u.ServeJson()
+func (this *UserController) RegisterEnd() {
+	u := models.User{}
+	if err := this.ParseForm(&u); err != nil {
+		//handle error
+		this.Rsp(false, err.Error())
+		return
+	}
+	id, err := models.RegisterEnd(&u)
+	if err == nil && id > 0 {
+		this.Rsp(true, "Success")
+		return
+	} else {
+		this.Rsp(false, err.Error())
+		return
+	}
+}
+
+func (this *UserController) UpdateProfile() {
+	u := models.User{}
+	if err := this.ParseForm(&u); err != nil {
+		//handle error
+		this.Rsp(false, err.Error())
+		return
+	}
+	id, err := models.UpdateProfile(&u)
+	if err == nil && id > 0 {
+		this.Rsp(true, "Success")
+		return
+	} else {
+		this.Rsp(false, err.Error())
+		return
+	}
+
+}
+
+func (this *UserController) DelUser() {
+	Id, _ := this.GetInt64("Id")
+	status, err := models.DelUserById(Id)
+	if err == nil && status > 0 {
+		this.Rsp(true, "Success")
+		return
+	} else {
+		this.Rsp(false, err.Error())
+		return
+	}
 }
 
 // @Title login
@@ -97,15 +84,22 @@ func (u *UserController) Delete() {
 // @Success 200 {string} login success
 // @Failure 403 user not exist
 // @router /login [get]
-func (u *UserController) Login() {
-	phone := u.GetUInt32("phone")
-	password := u.GetString("password")
-	if models.Login(phone, password) {
-		u.Data["json"] = "login success"
-	} else {
-		u.Data["json"] = "user not exist"
+func (this *UserController) Login() {
+	u := models.User{}
+	if err := this.ParseForm(&u); err != nil {
+		//handle error
+		this.Rsp(false, err.Error())
+		return
 	}
-	u.ServeJson()
+
+	err := models.Login(&u)
+	if err == nil {
+		this.Rsp(true, "Success")
+		return
+	} else {
+		this.Rsp(false, err.Error())
+		return
+	}
 }
 
 // @Title logout
--- routers/router.go
+++ routers/router.go
@@ -8,23 +8,66 @@
 package routers
 
 import (
-	"telepathy/controllers"
+	"fmt"
+	"os"
 
 	"github.com/astaxie/beego"
+	. "telepathy/Utils"
+	"telepathy/controllers"
+	"telepathy/models"
 )
 
 func init() {
-	ns := beego.NewNamespace("/v1",
-		beego.NSNamespace("/object",
-			beego.NSInclude(
-				&controllers.ObjectController{},
-			),
-		),
-		beego.NSNamespace("/user",
-			beego.NSInclude(
-				&controllers.UserController{},
-			),
-		),
-	)
-	beego.AddNamespace(ns)
+	//ns := beego.NewNamespace("/v1",
+	//	beego.NSNamespace("/object",
+	//		beego.NSInclude(
+	//			&controllers.ObjectController{},
+	//		),
+	//	),
+	//	beego.NSNamespace("/user",
+	//		beego.NSInclude(
+	//			&controllers.UserController{},
+	//		),
+	//	),
+	//)
+	//beego.AddNamespace(ns)
+
+	Run()
+}
+
+func Run() {
+	//初始化
+	initialize()
+
+	fmt.Println("Starting....")
+
+	fmt.Println("Start ok")
+}
+func initialize() {
+	//判断初始化参数
+	initArgs()
+
+	models.Connect()
+
+	router()
+	beego.AddFuncMap("stringsToJson", StringsToJson)
+}
+func initArgs() {
+	args := os.Args
+	for _, v := range args {
+		if v == "-syncdb" {
+			models.Syncdb()
+			os.Exit(0)
+		}
+	}
+}
+func router() {
+	beego.Router("/telepathy/user/login", &controllers.UserController{}, "*:Register")
+	beego.Router("/telepathy/user/logout", &controllers.UserController{}, "*:RegisterEnd")
+	beego.Router("/telepathy/user/changepwd", &controllers.UserController{}, "*:RegisterEnd")
+	beego.Router("/telepathy/user/register", &controllers.UserController{}, "*:Register")
+	beego.Router("/telepathy/user/registerend", &controllers.UserController{}, "*:RegisterEnd")
+	beego.Router("/telepathy/user/updateprofile", &controllers.UserController{}, "*:UpdateProfile")
+	beego.Router("/telepathy/user/deluser", &controllers.UserController{}, "*:DelUser")
+
 }
